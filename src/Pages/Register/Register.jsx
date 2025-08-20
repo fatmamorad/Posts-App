29fbff338ed7439a00791941943f2b3c2e810303
fatmamorad <fatmamahamoud0@gmail.com>
2025-08-20 18:10:00 +0300

    Last Updata

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -43,14 +43,14 @@ function Register() {
         <> 
         <div className='flex flex-wrap flex-col justify-center items-center'>
     
-            <div className='flex justify-centeritems-center h-screen  '> 
+            <div className='flex justify-centeritems-center h-screen  w-full '> 
                 <form onSubmit={handleSubmit((data)=>{
                     LoginMutation.mutate(data)
-                })} className=" md:w-96 sm:w-1/2  mx-auto flex justify-center items-center flex-col">
+                })} className=" md:w-96 w-1/2   mx-auto flex justify-center items-center flex-col">
                 <div className="relative z-0 w-full mb-5 group">
                     <input  type="text" {...register("email")} name="email" id="email"  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"placeholder=" " />
                     <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-                     {errors.email&&<p className='text-red-500 mt-3 text-xs ms-2 dark:text-red-300'> <i class=" dark:text-red-300 fa-solid text-red-600 fa-triangle-exclamation me-2"></i>{errors.email.message}</p>  }
+                     {errors.email&&<p className='text-red-500  mt-3 text-xs ms-2 dark:text-red-300'> <i class=" dark:text-red-300 fa-solid text-red-600 fa-triangle-exclamation me-2"></i>{errors.email.message}</p>  }
                      
                 </div>
                 
@@ -62,7 +62,7 @@ function Register() {
                     </div>
                
                 
-                  <p className='mb-3 dark:text-white'>Dont Have an Account ? <Link to='/Login' className='text-blue-600' >Register...</Link></p>
+                  <p className='mb-3 dark:text-white text-xs md:text-sm'>Dont Have an Account ? <Link to='/Login' className='text-blue-600' >Register...</Link></p>
                
                 
                 {!LoginMutation.isPending?<button type="submit" className="text-white btn hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>:
